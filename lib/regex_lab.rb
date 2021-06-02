a9657789867cb9bc19f087045c5b5d1a20f1b621
Wen Yu <wyu10amy@hotmail.com>
2021-06-02 04:52:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  scan(/[^a|e|i|o|u]/)
+  if word.scan(/^[aeiouAEIOU]/)
+    false
+  else
+    true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
